2eee8aa0577ad44e0873deeafecdb644d59fb393
MohamedHamdy28 <m.abdelhamid@innopolis.university>
2021-11-26 17:54:33 +0300

    added the state finished

---
+++ src/com/team35/restaurants/OrderFinished.java
@@ -0,0 +1,17 @@
+package com.team35.restaurants;
+// the state of the order should be changed to this after the order is delivered from the class application
+public class OrderFinished extends OrderState {
+    OrderFinished(Order theOrder) {
+        super(theOrder);
+    }
+
+    void cancelOrder(){
+        System.out.println("This order is already finished");
+    }
+    void refundOrder() {
+        //refund nothing
+    }
+    void changeOrderState(OrderState orderState){
+        System.out.println("This order is finished");
+    }
+}
